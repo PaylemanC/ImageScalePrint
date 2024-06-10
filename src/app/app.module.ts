0eb01ab1476153a9722f4b7dbdfd4610a39ebf34
Carina Payleman <rociocarina2@gmail.com>
2024-06-10 11:59:05 -0300

    Create main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
+import { UploadImageComponent } from './components/upload-image/upload-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    UploadImageComponent
   ],
   imports: [
     BrowserModule
