20eaf0f35901914e61af65f9b9af9639384bb345
Carina Payleman <rociocarina2@gmail.com>
2024-06-10 16:00:11 -0300

    Define model component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
-  templateUrl: './modal.component.html',
+  template: `
+  <div class="modal" [style.display]="isOpenModal ? 'block' : 'none'">
+    <div class="modal-content">
+      <div class="modal-close--container">
+        <button class="modal-close" (click)="closeModal()"></button>
+      </div>
+      <ng-content></ng-content>
+    </div>
+  </div>`,
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
+  @Input() isOpenModal: boolean = false;
+  @Output() modalClosed = new EventEmitter<void>();
 
+  closeModal() {
+    this.isOpenModal = false;
+    this.modalClosed.emit();
+  }
 }
