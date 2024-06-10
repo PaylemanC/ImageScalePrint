4a5cbbe3c2b1aad693fb67b04cae60153622f60b
Carina Payleman <rociocarina2@gmail.com>
2024-06-10 14:12:55 -0300

    Create reusable modal & tutorial component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './components/canvas/canvas.component';
 import { UploadImageComponent } from './components/upload-image/upload-image.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CanvasComponent,
-    UploadImageComponent
+    UploadImageComponent,
+    ModalComponent,
+    TutorialComponent
   ],
   imports: [
     BrowserModule
