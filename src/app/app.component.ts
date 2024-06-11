6a5e842c22f1f73d2f62d84b50ed3c827a48449f
Carina Payleman <rociocarina2@gmail.com>
2024-06-10 22:46:25 -0300

    Add download pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { fabric } from 'fabric';
+import { jsPDF } from 'jspdf';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,66 @@ export class AppComponent {
   title: string = 'Título';
   orientation: string = 'horizontal';
   borders: string = '';
+
+  constructor(private toastr: ToastrService, private cdr: ChangeDetectorRef) {}
+
+  async downloadPDF() {
+    this.toastr.info('Espere un momento por favor.', 'Descarga en proceso.', {
+      tapToDismiss: false,
+      positionClass: 'toast-bottom-right',
+      newestOnTop: false
+    })
+
+    this.cdr.detectChanges();
+
+    try {
+      const pdfOrientation = this.orientation === 'horizontal' ? 'l' : 'p';
+      const pdf = new jsPDF(pdfOrientation, 'mm', 'a4');
+
+      const dashedLines: fabric.Line[] = [];
+      this.canvas?.getObjects().forEach(obj => {
+        if (obj instanceof fabric.Line && obj.strokeDashArray?.[0] === 5) {
+          dashedLines.push(obj);
+        }
+      });
+
+      const clonedCanvas = fabric.util.object.clone(this.canvas!);
+      clonedCanvas.getObjects().forEach((obj: any) => {
+        if (obj instanceof fabric.Line) {
+          clonedCanvas.remove(obj);
+        }
+      });
+
+      const imageData = clonedCanvas.toDataURL({ format: 'jpeg', quality: 1.0 });
+      const imgWidth = pdfOrientation === 'l' ? 297 : 210;
+      const imgHeight = (clonedCanvas.height * imgWidth) / clonedCanvas.width;
+      pdf.addImage(imageData, 'JPEG', 0, 0, imgWidth, imgHeight);
+
+      if (this.title.trim() === '') {
+        this.title = '(sin título)';
+      }
+
+      await pdf.save(`${this.title}, ${this.borders}, ${this.orientation}.pdf`);
+
+      this.toastr.success('Tu archivo se descargará con éxito.', 'Descarga completada.', {
+        tapToDismiss: true,
+        positionClass: 'toast-bottom-right',
+        newestOnTop: false
+      })
+
+      dashedLines.forEach(line => {
+        this.canvas?.add(line);
+      });
+      this.canvas?.renderAll();
+
+    } catch (error) {
+      this.toastr.error('Ha ocurrido un error al intentar descargar tu archivo. Pruebe nuevamente en unos minutos', 'Error inesperado', {
+        tapToDismiss: true,
+        positionClass: 'toast-bottom-right',
+        newestOnTop: false
+      });
+    }
+  }
 
   showModalImage: boolean = false;
   showModalTutorial: boolean = false;
